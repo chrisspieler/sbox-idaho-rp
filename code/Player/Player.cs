8b49358cf0432282f412fc99200a2bbe0201f8bf
Chris Spieler <spielerc@gmail.com>
2023-02-04 09:54:15 -0700

    Added features to test magic

--- code/Player/Player.cs
+++ code/Player/Player.cs
@@ -66,13 +66,31 @@ public partial class Idahoid : AnimatedEntity
 		Components.Create<InteractionMechanic>();
 	}
 
-	[ConCmd.Admin("sethealth")]
-	public static void SetHealth(float value )
+	[ConCmd.Admin( "sethealth" )]
+	public static void SetHealth( float value ) => ConsoleSetStat( PlayerStat.Health, value );
+
+	[ConCmd.Admin( "setmagic" )]
+	public static void SetMagic( float value ) => ConsoleSetStat( PlayerStat.Magic, value );
+
+	[ConCmd.Admin("setstat")]
+	public static void SetStat(string statName, float value )
+	{
+		if (Enum.TryParse(typeof(PlayerStat), statName, true, out object stat))
+		{
+			ConsoleSetStat( (PlayerStat)stat, value );
+		}
+		else
+		{
+			Log.Error( $"Invalid stat name: \"{statName}\"" );
+		}
+	}
+
+	private static void ConsoleSetStat(PlayerStat stat, float value)
 	{
 		var player = ConsoleSystem.Caller.Pawn as Idahoid;
 		if ( player == null )
 			return;
-		player.Health = value;
+		player.SetStat( stat, value );
 	}
 
 	public override void Simulate( IClient cl )
@@ -81,6 +99,47 @@ public partial class Idahoid : AnimatedEntity
 
 		TickRegen();
 		TickStatChanges();
+		ExecuteMagicTest();
+	}
+
+	private TimeUntil _canLeftClick;
+	private TimeUntil _canRightClick;
+
+	private void ExecuteMagicTest()
+	{
+		if ( Game.IsServer )
+		{
+			if ( Input.Pressed( InputButton.PrimaryAttack ) && _canLeftClick && TrySpendMagic(5.0f) )
+			{
+				LaunchModel( "models/sbox_props/watermelon/watermelon.vmdl_c" );
+				_canLeftClick = 1.0f;
+			}
+			if ( Input.Pressed( InputButton.SecondaryAttack ) && _canRightClick && TrySpendMagic(20.0f) )
+			{
+				LaunchModel( "models/citizen/citizen.vmdl" );
+				_canRightClick = 3.0f;
+			}
+			
+		}
+
+		bool TrySpendMagic(float amount )
+		{
+			var currentMagic = GetStat( PlayerStat.Magic );
+			if ( currentMagic < amount )
+				return false;
+			OffsetStat( PlayerStat.Magic, -amount );
+			return true;
+		}
+
+		void LaunchModel(string modelPath )
+		{
+			var model = new ModelEntity();
+			model.SetModel( modelPath );
+			model.Position = EyePosition + EyeRotation.Forward * 40;
+			model.Rotation = Rotation.LookAt( Vector3.Random.Normal );
+			model.SetupPhysicsFromModel( PhysicsMotionType.Dynamic, false );
+			model.PhysicsGroup.Velocity = EyeRotation.Forward * 1000;
+		}
 	}
 
 	public override void FrameSimulate( IClient cl )
